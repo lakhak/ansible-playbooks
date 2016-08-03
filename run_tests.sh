5fd30b3a4d600d36f70a88ec6ff875b7de221ad5
lakhak <krishan.lakha@gmail.com>
2016-08-03 22:56:13 +0100

    Adding playbook for installing Zookeeper

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,4 @@
 
 #docker run -it -v $PWD:/ansible 8689750be317 /bin/bash
 docker run -v $PWD:/ansible 8689750be317 \
-    ansible-playbook -i roles/java/test/inventory roles/java/test/main.yml
+    ansible-playbook -i docker/inventory roles/java/test/main.yml
